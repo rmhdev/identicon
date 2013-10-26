02ce9ba79837e810be2e7bafc16bfa11e1610463
Rober Martín H <rmh.dev@gmail.com>
2013-10-26 20:04:50 +0200

    Remove test images. Calculate correct center of boxes in tests.

--- tests/Identicon/Tests/IdenticonTest.php
+++ tests/Identicon/Tests/IdenticonTest.php
@@ -48,8 +48,8 @@ class IdenticonTest extends \PHPUnit_Framework_TestCase
         for ($x = 0; $x < $length; $x++) {
             for ($y = 0; $y < $length; $y++) {
                 $backgroundColor = (string) $identicon->getBackgroundColor();
-                $centerX = 30 + $y*70 + 70/2;
-                $centerY = 30 + $x*70 + 70/2;
+                $centerX = 35 + $y*70 + 70/2;
+                $centerY = 35 + $x*70 + 70/2;
                 $color = $image->getColorAt(new Point($centerX, $centerY));
                 $comment = "position [{$x}, {$y}]: {$centerX}px, {$centerY}px";
                 if ($identicon->getIdentity()->getBlock($x, $y)->isColored()) {
@@ -59,7 +59,7 @@ class IdenticonTest extends \PHPUnit_Framework_TestCase
                 }
             }
         }
-        //unlink($filename);
+        unlink($filename);
     }
 
     protected function createFile(Identicon $identicon)
