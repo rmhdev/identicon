fd1ad1159cc60f49709e3b35206388533e943579
Rober Martín H <rmh.dev@gmail.com>
2013-12-29 12:31:38 +0100

    Square type does nothing because of inheritance.

--- src/Identicon/Types/Square/Identicon.php
+++ src/Identicon/Types/Square/Identicon.php
@@ -7,24 +7,5 @@ use Imagine\Image\Point;
 
 class Identicon extends AbstractIdenticon
 {
-    protected function drawBlock($x, $y)
-    {
-        $this->image->draw()->polygon(
-            $this->calculatePolygonCoordinates($x, $y),
-            $this->getColor(),
-            true
-        );
-    }
 
-    protected function calculatePolygonCoordinates($x, $y)
-    {
-        $cell = $this->getCell($x, $y);
-
-        return array(
-            $cell->getNorthWest(),
-            $cell->getNorthEast(),
-            $cell->getSouthEast(),
-            $cell->getSouthWest(),
-        );
-    }
 }
