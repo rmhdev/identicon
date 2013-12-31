c5b31e83500bf974f98f4723f0700cf81a8b3a54
Rober Martín H <rmh.dev@gmail.com>
2013-12-31 12:00:46 +0100

    Refactoring controllers.

--- src/Identicon/Controller/BaseController.php
+++ src/Identicon/Controller/BaseController.php
@@ -5,6 +5,8 @@ namespace Identicon\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Silex\Application;
+use Identicon\Type\Square\Identicon;
+use Identicon\Identity\Identity;
 
 /**
  * Based on https://igor.io/2012/11/09/scaling-silex.html
@@ -22,6 +24,57 @@ class BaseController
         }
         $response = new Response(
             $app["twig"]->render("index.twig", array("error" => $name ? false : true)),
+            200,
+            array(
+                "Cache-Control" => "public, max-age=3600, s-maxage=3600"
+            )
+        );
+        $response->isNotModified($request);
+        $response->setEtag(md5($response->getContent()));
+
+        return $response;
+    }
+
+    public function basicAction(Request $request, Application $app)
+    {
+        $name = $request->request->get("name");
+        $identicon = new Identicon($name);
+        $response = new Response($identicon->getContent(), 200, array(
+            "Content-Type" => "image/png",
+            "Cache-Control" => "public, max-age=3600, s-maxage=3600"
+        ));
+        $response->isNotModified($request);
+        $response->setEtag(md5($response->getContent()));
+
+        return $response;
+    }
+
+    public function extraAction(Request $request, $type, $name)
+    {
+        $class = sprintf('\Identicon\Type\%s\Identicon', ucfirst($type));
+        if (!class_exists($class)) {
+            return new Response("Error", 404);
+        }
+        $identicon = new $class($name);
+        /* @var \Identicon\AbstractIdenticon $identicon */
+        $response = new Response($identicon->getContent(), 200, array(
+            "Content-Type" => "image/png",
+            "Cache-Control" => "public, max-age=3600, s-maxage=3600"
+        ));
+        $response->isNotModified($request);
+        $response->setEtag(md5($response->getContent()));
+
+        return $response;
+    }
+
+    public function profileAction(Request $request, Application $app, $name)
+    {
+        $identity = new Identity($name);
+        $response = new Response(
+            $app["twig"]->render("profile.twig", array(
+                "name" => $identity->getName(),
+                "types" => array("pyramid", "circle", "rhombus")
+            )),
             200,
             array(
                 "Cache-Control" => "public, max-age=3600, s-maxage=3600"
--- src/controllers.php
+++ src/controllers.php
@@ -1,9 +1,6 @@
 <?php
 
 use \Symfony\Component\HttpFoundation\Response;
-use \Symfony\Component\HttpFoundation\Request;
-use Identicon\Identity\Identity;
-use \Identicon\Type\Square\Identicon;
 
 /* @var \Silex\Application $app
  * @return Response
@@ -12,49 +9,11 @@ use \Identicon\Type\Square\Identicon;
 $app->match("/",
     'Identicon\Controller\BaseController::indexAction')->bind("index");
 
-$app->get("/{name}.png", function(Request $request, $name) use ($app) {
-    $identicon = new Identicon($name);
-    $response = new Response($identicon->getContent(), 200, array(
-        "Content-Type" => "image/png",
-        "Cache-Control" => "public, max-age=3600, s-maxage=3600"
-    ));
-    $response->isNotModified($request);
-    $response->setEtag(md5($response->getContent()));
+$app->get("/{name}.png",
+    'Identicon\Controller\BaseController::basicAction')->bind("basic");
 
-    return $response;
-})->bind("basic");
+$app->get("/{name}/{type}.png",
+    'Identicon\Controller\BaseController::extraAction')->bind("extra");
 
-$app->get("/{name}/{type}.png", function(Request $request, $name, $type) use ($app) {
-    $class = sprintf('\Identicon\Type\%s\Identicon', ucfirst($type));
-    if (!class_exists($class)) {
-        return new Response("Error", 404);
-    }
-    $identicon = new $class($name);
-    /* @var \Identicon\AbstractIdenticon $identicon */
-    $response = new Response($identicon->getContent(), 200, array(
-        "Content-Type" => "image/png",
-        "Cache-Control" => "public, max-age=3600, s-maxage=3600"
-    ));
-    $response->isNotModified($request);
-    $response->setEtag(md5($response->getContent()));
-
-    return $response;
-})->bind("extra");
-
-$app->get("/{name}", function(Request $request, $name) use ($app) {
-    $identity = new Identity($name);
-    $response = new Response(
-        $app["twig"]->render("profile.twig", array(
-            "name" => $identity->getName(),
-            "types" => array("pyramid", "circle", "rhombus")
-        )),
-        200,
-        array(
-            "Cache-Control" => "public, max-age=3600, s-maxage=3600"
-        )
-    );
-    $response->isNotModified($request);
-    $response->setEtag(md5($response->getContent()));
-
-    return $response;
-});
+$app->get("/{name}",
+    'Identicon\Controller\BaseController::profileAction')->bind("profile");
