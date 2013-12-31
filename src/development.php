ad90d6491208361d9122eb14d4f661158319985a
Rober Martín H <rmh.dev@gmail.com>
2013-12-31 13:35:38 +0100

    Add config service provider.

--- src/development.php
+++ src/development.php
@@ -1,9 +1,10 @@
 <?php
 require_once __DIR__ . "/../vendor/autoload.php";
 
+putenv("APP_ENV=dev");
+
 /* @var \Silex\Application $app */
 $app = require __DIR__ . "/app.php";
-$app["debug"] = true;
 
 require __DIR__ . "/providers.php";
 require __DIR__ . "/controllers.php";
--- src/providers.php
+++ src/providers.php
@@ -11,3 +11,10 @@ $app->register(new Silex\Provider\HttpCacheServiceProvider(), array(
     'http_cache.cache_dir'  => __DIR__ . "/../cache/",
     'http_cache.esi'        => null
 ));
+
+$env = getenv('APP_ENV') ?: 'prod';
+$folder = __DIR__ . "/../config/";
+$file = file_exists($folder . "$env.json") ? "$env.json" : "$env.json.dist";
+$app->register( new Igorw\Silex\ConfigServiceProvider($folder . $file), array(
+
+));
