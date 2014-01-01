66ae9309ed22b474a7f949402d804a0751d89e19
Rober Martín H <rmh.dev@gmail.com>
2014-01-01 23:17:55 +0100

    Empty configuration values in test environment: Identicon will use default values.

--- src/env_test.php
+++ src/env_test.php
@@ -6,13 +6,7 @@ require_once __DIR__ . "/../vendor/autoload.php";
 /* @var \Silex\Application $app */
 $app = require __DIR__ . "/app.php";
 
-$app["identicon.config"] = array_merge(
-    $app["identicon.config"], array(
-        "blocks" => AbstractIdenticon::BLOCKS,
-        "block-size" => AbstractIdenticon::BLOCK_SIZE,
-        "margin" => AbstractIdenticon::MARGIN,
-        "background-color" => AbstractIdenticon::BACKGROUND_COLOR
-));
+$app["identicon.config"] = array();
 $app["identicon.type"] = array_merge(
     $app["identicon.type"], array(
         "default" => "square"
